65542e797a0f89c0fb391fdb0cd0c6d7564986f6
Hesham Salman <iron-ham@github.com>
2020-01-26 18:37:29 -0500

    Initial Commit

---
+++ ActionsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActionsDemo
+//
+//  Created by Hesham Salman on 1/26/20.
+//  Copyright © 2020 heshamsalman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
